c73b074cca847b6436d53d1c56a9b3d773b5cd53
srcnert <sercanerat@gmail.com>
2025-11-13 00:55:38 +0300

    Adding rak11160 tcxo voltage.

--- app/lora/lora_shell/src/cli.c
+++ app/lora/lora_shell/src/cli.c
@@ -5,15 +5,15 @@
 
 static int cmd_test_example(const struct shell *shell, size_t argc, char *argv[])
 {
-	shell_print(shell, "Testing cmd!!!");
+	shell_print(shell, "Board: %s", CONFIG_BOARD);
 	return 0;
 }
 
-SHELL_STATIC_SUBCMD_SET_CREATE(test_cli_cmds,
+SHELL_STATIC_SUBCMD_SET_CREATE(test_cmds,
 
 			       SHELL_CMD_ARG(test, NULL, "trigger a test command", cmd_test_example,
 					     0 /* mandatory param */, 0 /* optional param */),
 
 			       SHELL_SUBCMD_SET_END);
 
-SHELL_CMD_REGISTER(test_cli, &test_cli_cmds, "TEST CLI", NULL);
+SHELL_CMD_REGISTER(rak, &test_cmds, "RAKwireless", NULL);
